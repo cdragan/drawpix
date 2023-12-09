572ce83fe7dd4fd4d4ebcb2b815296f1bbed287c
Chris Dragan <chris@dragan.dev>
2023-12-09 17:58:06 +0000

    refactor to use prototype objects

--- app.js
+++ app.js
@@ -42,23 +42,31 @@ function Elem(id)
     this.elem = (typeof id === "string") ? document.getElementById(id) : id;
 }
 
-Elem.prototype.setContents = function(contents)
-{
-    this.elem.innerHTML = contents;
-    return this;
-};
+Elem.prototype = {
 
-Elem.prototype.setAttr = function(name, value)
-{
-    const attr = document.createAttribute(name);
-    attr.value = value;
-    this.elem.setAttributeNode(attr);
-    return this;
+    setContents: function(contents)
+    {
+        this.elem.innerHTML = contents;
+        return this;
+    },
+
+    setAttr: function(name, value)
+    {
+        const attr = document.createAttribute(name);
+        attr.value = value;
+        this.elem.setAttributeNode(attr);
+        return this;
+    }
 };
 
 function E(id)
 {
     return (id instanceof Elem) ? id : new Elem(id);
+}
+
+function SvgColor(color)
+{
+    return (color[0] === "#") ? ('style="fill: ' + color + '"') : ('class="' + color + '"');
 }
 
 function DrawSvg(id)
@@ -67,28 +75,26 @@ function DrawSvg(id)
     this.contents = "";
 }
 
-DrawSvg.prototype.Commit = function()
-{
-    this.elem.setContents(this.contents);
-    this.contents = "";
-};
+DrawSvg.prototype = {
 
-function SvgColor(color)
-{
-    return (color[0] === "#") ? ('style="fill: ' + color + '"') : ('class="' + color + '"');
-}
+    Commit: function()
+    {
+        this.elem.setContents(this.contents);
+        this.contents = "";
+    },
 
-DrawSvg.prototype.Rect = function(id, color, x, y, width, height)
-{
-    id = id ? ('id="' + id + '"') : "";
-    this.contents += '<rect ' + id + ' ' +
-                           SvgColor(color) + " " +
-                           'x="' + x + '" ' +
-                           'y="' + y + '" ' +
-                           'width="' + width + '" ' +
-                           'height="' + height + '" ' +
-                           '/>';
-    return this;
+    Rect: function(id, color, x, y, width, height)
+    {
+        id = id ? ('id="' + id + '"') : "";
+        this.contents += '<rect ' + id + ' ' +
+                               SvgColor(color) + " " +
+                               'x="' + x + '" ' +
+                               'y="' + y + '" ' +
+                               'width="' + width + '" ' +
+                               'height="' + height + '" ' +
+                               '/>';
+        return this;
+    }
 };
 
 function GetDimension(id)
@@ -106,84 +112,87 @@ function Editor(id)
     this.img_height = 0;
 }
 
-Editor.prototype.Update = function()
-{
-    this.img_width  = GetDimension("img_width")  || this.img_width;
-    this.img_height = GetDimension("img_height") || this.img_height;
+Editor.prototype = {
 
-    // Set height of image-container and image to match the width
-    const elem = E("image-container");
-    const width  = Math.floor(elem.elem.offsetWidth);
-    const height = Math.floor(width * this.img_height / this.img_width);
-    elem.elem.style.height = height + "px";
-    this.elem.setAttr("width", width);
-    this.elem.setAttr("height", height);
-    this.elem.setAttr("viewbox", "0 0 " + width + " " + height);
+    Update: function()
+    {
+        this.img_width  = GetDimension("img_width")  || this.img_width;
+        this.img_height = GetDimension("img_height") || this.img_height;
 
-    this.Draw();
-};
+        // Set height of image-container and image to match the width
+        const elem = E("image-container");
+        const width  = Math.floor(elem.elem.offsetWidth);
+        const height = Math.floor(width * this.img_height / this.img_width);
+        elem.elem.style.height = height + "px";
+        this.elem.setAttr("width", width);
+        this.elem.setAttr("height", height);
+        this.elem.setAttr("viewbox", "0 0 " + width + " " + height);
 
-Editor.prototype.Draw = function()
-{
-    const rect        = this.elem.elem.getBoundingClientRect();
-    const phys_width  = rect.right - rect.left;
-    const phys_height = rect.bottom - rect.top;
-    const cell_width  = Math.floor((phys_width - pad_size) / this.img_width);
-    const cell_height = Math.floor((phys_height - pad_size) / this.img_height);
+        this.Draw();
+    },
 
-    const svg = new DrawSvg(this.elem);
+    Draw: function()
+    {
+        const rect        = this.elem.elem.getBoundingClientRect();
+        const phys_width  = rect.right - rect.left;
+        const phys_height = rect.bottom - rect.top;
+        const cell_width  = Math.floor((phys_width - pad_size) / this.img_width);
+        const cell_height = Math.floor((phys_height - pad_size) / this.img_height);
 
-    for (let x = 0; x <= this.img_width; x++) {
-        svg.Rect("", "grid", x * cell_width, 0, pad_size, this.img_height * cell_height + pad_size);
-    }
-    for (let y = 0; y <= this.img_height; y++) {
-        svg.Rect("", "grid", 0, y * cell_height, this.img_width * cell_width, pad_size);
-    }
+        const svg = new DrawSvg(this.elem);
 
-    svg.Rect("sel-bg", "sel-bg", 0, 0, cell_width + pad_size, cell_height + pad_size);
-
-    for (let y = 0; y < this.img_height; y++) {
-        for (let x = 0; x < this.img_width; x++) {
-            svg.Rect("ed_" + x + "_" + y,
-                     "#00000000",
-                     x * cell_width + pad_size,
-                     y * cell_height + pad_size,
-                     cell_width - pad_size,
-                     cell_height - pad_size);
+        for (let x = 0; x <= this.img_width; x++) {
+            svg.Rect("", "grid", x * cell_width, 0, pad_size, this.img_height * cell_height + pad_size);
         }
+        for (let y = 0; y <= this.img_height; y++) {
+            svg.Rect("", "grid", 0, y * cell_height, this.img_width * cell_width, pad_size);
+        }
+
+        svg.Rect("sel-bg", "sel-bg", 0, 0, cell_width + pad_size, cell_height + pad_size);
+
+        for (let y = 0; y < this.img_height; y++) {
+            for (let x = 0; x < this.img_width; x++) {
+                svg.Rect("ed_" + x + "_" + y,
+                         "#00000000",
+                         x * cell_width + pad_size,
+                         y * cell_height + pad_size,
+                         cell_width - pad_size,
+                         cell_height - pad_size);
+            }
+        }
+
+        svg.Commit();
+
+        E("sel-bg").setAttr("visibility", "hidden");
+        E("sel-bg").setAttr("class", "sel-bg");
+
+        this.cell_width  = cell_width;
+        this.cell_height = cell_height;
+    },
+
+    OnMouseMove: function(client_x, client_y)
+    {
+        const rect = this.elem.elem.getBoundingClientRect();
+        const x    = Math.floor((client_x - rect.x) / this.cell_width);
+        const y    = Math.floor((client_y - rect.y) / this.cell_height);
+
+        const sel_bg = E("sel-bg");
+
+        if (x < 0 || y < 0 || x >= this.img_width || y >= this.img_height) {
+            sel_bg.setAttr("visibility", "hidden");
+            return;
+        }
+
+        sel_bg.setAttr("visibility", "visible");
+        sel_bg.setAttr("transform", "translate(" + (x * this.cell_width) + "," + (y * this.cell_height) + ")");
+    },
+
+    OnMouseClick: function(client_x, client_y)
+    {
+        const rect = this.elem.elem.getBoundingClientRect();
+        const x    = Math.floor((client_x - rect.x) / this.cell_width);
+        const y    = Math.floor((client_y - rect.y) / this.cell_height);
     }
-
-    svg.Commit();
-
-    E("sel-bg").setAttr("visibility", "hidden");
-    E("sel-bg").setAttr("class", "sel-bg");
-
-    this.cell_width  = cell_width;
-    this.cell_height = cell_height;
-};
-
-Editor.prototype.OnMouseMove = function(client_x, client_y)
-{
-    const rect = this.elem.elem.getBoundingClientRect();
-    const x    = Math.floor((client_x - rect.x) / this.cell_width);
-    const y    = Math.floor((client_y - rect.y) / this.cell_height);
-
-    const sel_bg = E("sel-bg");
-
-    if (x < 0 || y < 0 || x >= this.img_width || y >= this.img_height) {
-        sel_bg.setAttr("visibility", "hidden");
-        return;
-    }
-
-    sel_bg.setAttr("visibility", "visible");
-    sel_bg.setAttr("transform", "translate(" + (x * this.cell_width) + "," + (y * this.cell_height) + ")");
-};
-
-Editor.prototype.OnMouseClick = function(client_x, client_y)
-{
-    const rect = this.elem.elem.getBoundingClientRect();
-    const x    = Math.floor((client_x - rect.x) / this.cell_width);
-    const y    = Math.floor((client_y - rect.y) / this.cell_height);
 };
 
 function Preview(id, editor)
@@ -192,19 +201,22 @@ function Preview(id, editor)
     this.editor = editor;
 }
 
-Preview.prototype.Draw = function()
-{
-    const img_width  = this.editor.img_width;
-    const img_height = this.editor.img_height;
+Preview.prototype = {
 
-    const canvas = document.createElement("canvas");
-    canvas.width  = img_width;
-    canvas.height = img_height;
+    Draw: function()
+    {
+        const img_width  = this.editor.img_width;
+        const img_height = this.editor.img_height;
 
-    const ctx = canvas.getContext("2d");
+        const canvas = document.createElement("canvas");
+        canvas.width  = img_width;
+        canvas.height = img_height;
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, img_width, img_height);
+        const ctx = canvas.getContext("2d");
 
-    this.elem.elem.src = canvas.toDataURL();
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, img_width, img_height);
+
+        this.elem.elem.src = canvas.toDataURL();
+    }
 };
